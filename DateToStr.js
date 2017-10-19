b1bb939de1495f45abe21a427fe79bcbe2e6500d
maxtang <yllfever@gmail.com>
2017-10-19 09:29:21 +0800

    Test2

--- DateToStr.js
+++ DateToStr.js
@@ -22,9 +22,9 @@ console.log(formatDateMonthDay(str));
 console.log(new Date(str).getTime());
 
 var a = [];
-            for (var cur = 1; cur < 5; cur += 1) {
-                a.push(cur);
-            }
+for (var cur = 1; cur < 5; cur += 1) {
+	a.push(cur);
+}
 
 console.log(a.length);
 
